3a9656c17207b1d8de612244aa70c47c8296dec7
Albert <albert.foreber@gmail.com>
2025-07-22 17:56:38 +0500

    Merged with katrinbe

--- app/src/main/java/com/example/sdo_project/domain/repository/GradesRepository.kt
+++ app/src/main/java/com/example/sdo_project/domain/repository/GradesRepository.kt
@@ -4,8 +4,6 @@ import com.example.sdo_project.domain.models.GradePoint
 import com.example.sdo_project.domain.models.GradeSection
 
 interface GradesRepository {
-    suspend fun getSectionsGradesByStudentId (student_id: Int, discipline_id: Int) : Result<GradeSection> // Grades Screen - main section (Attendance, Cur Control...) | Нужно получить все про секцию + балл counting from PointGrade - Grade Table
-    suspend fun getSectionGradesByStudentId (student_id: Int, section_id: Int): Result<List<GradePoint>> //Attendance ->  work1 .., work 2 .. | Получаем id, name, max_grade; cur_grade from Grade Table
-    suspend fun getPointGradeByStudentId (student_id: Int,discipline_id: Int, point_id: Int ): Result<GradePoint> // Work1 ...  9/10 on the Grade Screen
-    //suspend fun getFullPointGradeByStudentId (student_id: Int, point_id: Int ): Result <> // Screen for an a Point Screen
+    suspend fun getSectionsGradesByStudentId(studentUuid: String, disciplineId: Int): Result<List<GradeSection>> // Grades Screen - main section (Attendance, Cur Control...) | Нужно получить все про секцию + балл counting from PointGrade - Grade Table
+    suspend fun getSectionGradesByStudentId(studentUuid: String, sectionId: Int, disciplineId: Int): Result<List<GradePoint>> //Attendance ->  work1 .., work 2 .. | Получаем id, name, max_grade; cur_grade from Grade Table
 }
--- app/src/main/java/com/example/sdo_project/domain/repository/MaterialRepository.kt
+++ app/src/main/java/com/example/sdo_project/domain/repository/MaterialRepository.kt
@@ -6,5 +6,5 @@ import com.example.sdo_project.domain.models.MaterialSection
 interface MaterialRepository {
     suspend fun getMaterials(parent: MaterialSection): Result<List<Material>>           // Материалы в текущей секции (раздел, подраздел)
     suspend fun addMaterial(material: Material): Result<Unit>                           // Добавление материала (дата-класс содержит все необходимое)
-    suspend fun getMaterialSections(disciplineId: Int): Result<List<MaterialSection>>   // Получаем относительно id дисциплины
+    suspend fun getMaterialSections(disciplineId: Int, parentId: Int? = null): Result<List<MaterialSection>>   // Получаем относительно id дисциплины
 }
